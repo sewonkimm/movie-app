a4a7e596c026dffea675bbd5a04fa6d1ce346aae
Siromom <swon962@gmail.com>
2019-07-10 17:25:22 +0900

    fetch movie api

--- src/App.js
+++ src/App.js
@@ -1,50 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
-import { loadPartialConfig } from '@babel/core';
 
 class App extends Component {
 
   state = {
-    
   }
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title: "movie1",
-            poster: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjvsp771KnjAhWJurwKHdWaBBQQjRx6BAgBEAU&url=https%3A%2F%2Fmyhotposters.com%2Fproducts%2Fready-player-one-movie-poster-101483&psig=AOvVaw06FxwT2HE5yUSv5rCyq6Yd&ust=1562824464369719"
-          },
-          {
-            title: "movie2",
-            poster: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjh-dv_1KnjAhWBvLwKHV3dBH0QjRx6BAgBEAU&url=http%3A%2F%2Fwww.impawards.com%2F2019%2Faladdin_ver2.html&psig=AOvVaw06FxwT2HE5yUSv5rCyq6Yd&ust=1562824464369719"
-          },
-          {
-            title: "movie3",
-            poster: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjgjvGB1anjAhWLerwKHQ4AD98QjRx6BAgBEAU&url=%2Furl%3Fsa%3Di%26source%3Dimages%26cd%3D%26ved%3D%26url%3Dhttps%253A%252F%252Fwww.movieposter.com%252Fposter%252FMPW-121006%252FBlade_Runner_2049.html%26psig%3DAOvVaw06FxwT2HE5yUSv5rCyq6Yd%26ust%3D1562824464369719&psig=AOvVaw06FxwT2HE5yUSv5rCyq6Yd&ust=1562824464369719"
-          },
-          {
-            title: "movie4",
-            poster: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiP9rKH1anjAhXIV7wKHdO0AA8QjRx6BAgBEAU&url=https%3A%2F%2Fwww.joblo.com%2Fmovie-posters&psig=AOvVaw06FxwT2HE5yUSv5rCyq6Yd&ust=1562824464369719"
-          },
-          {
-            title: "new movie",
-            poster: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjvsp771KnjAhWJurwKHdWaBBQQjRx6BAgBEAU&url=https%3A%2F%2Fmyhotposters.com%2Fproducts%2Fready-player-one-movie-poster-101483&psig=AOvVaw06FxwT2HE5yUSv5rCyq6Yd&ust=1562824464369719"
-          }
-        ]
-      })
-    }, 3000);
+    this._getMovies();
   }
 
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    const movies = this.state.movies.map((movie) => {
+      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id} />
     });
 
     return movies;
   }
+
+   _getMovies = async() => {
+    const movies = await this._callApi();
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.lt/api/v2/list_movies.json?quality=3D?sort_by=rating')
+    .then(response => response.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
+  }
+
   render() {
     return (
       <div className="App">
